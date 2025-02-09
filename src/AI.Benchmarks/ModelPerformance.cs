cf8a1bfbe96fa6acca00bec2a388e90957267540
Daniel Cazzulino <daniel@cazzulino.com>
2025-02-09 20:03:49 -0300

    Add grok-2 and update to lates M.E.AI dependencies

--- src/AI.Benchmarks/ModelPerformance.cs
+++ src/AI.Benchmarks/ModelPerformance.cs
@@ -25,7 +25,7 @@ public class ModelPerformance
         new ChatMessage(ChatRole.User, "What is the meaning of life, the universe, and everything?"),
     ];
 
-    [Params("oai-gpt-4o", "oai-gpt-4o-mini", "aai-gpt-4o", "aai-gpt-4o-mini", "xai-grok-beta")]
+    [Params("oai-gpt-4o", "oai-gpt-4o-mini", "aai-gpt-4o", "aai-gpt-4o-mini", "xai-grok-beta", "xai-grok-2-latest")]
     public string? Client { get; set; }
 
     [GlobalSetup]
@@ -35,31 +35,41 @@ public class ModelPerformance
             .AddUserSecrets<ModelPerformance>()
             .Build();
 
-        services = new ServiceCollection()
-            .AddKeyedChatClient("oai-gpt-4o", builder => builder
-                .Use(new OpenAI.OpenAIClient(configuration.ǃ("OpenAI:Key")).AsChatClient("gpt-4o")))
-            .AddKeyedChatClient("oai-gpt-4o-mini", builder => builder
-                .Use(new OpenAI.OpenAIClient(configuration.ǃ("OpenAI:Key")).AsChatClient("gpt-4o-mini")))
-            .AddKeyedChatClient("aai-gpt-4o", builder => builder
-                .Use(new AzureAIInferenceChatClient(
-                    new Azure.AI.Inference.ChatCompletionsClient(
-                        new Uri(configuration.ǃ("AzureAI:Endpoint")),
-                        new AzureKeyCredential(configuration.ǃ("AzureAI:Key"))),
-                    "gpt-4o")))
-            .AddKeyedChatClient("aai-gpt-4o-mini", builder => builder
-                .Use(new AzureAIInferenceChatClient(
-                    new Azure.AI.Inference.ChatCompletionsClient(
-                        new Uri(configuration.ǃ("AzureAI:Endpoint")),
-                        new AzureKeyCredential(configuration.ǃ("AzureAI:Key"))),
-                    "gpt-4o-mini")))
-            .AddKeyedChatClient("xai-grok-beta", builder => builder
-                .Use(new OpenAI.OpenAIClient(
-                    new ApiKeyCredential(configuration.ǃ("xAI:Key")),
-                    new OpenAI.OpenAIClientOptions
-                    {
-                        Endpoint = new(configuration.ǃ("xAI:Endpoint"))
-                    }).AsChatClient("grok-beta")))
-            .BuildServiceProvider();
+        var collection = new ServiceCollection();
+        collection.AddKeyedChatClient("oai-gpt-4o", 
+            new OpenAI.OpenAIClient(configuration.ǃ("OpenAI:Key")).AsChatClient("gpt-4o"));
+        collection.AddKeyedChatClient("oai-gpt-4o-mini", 
+            new OpenAI.OpenAIClient(configuration.ǃ("OpenAI:Key")).AsChatClient("gpt-4o-mini"));
+
+        collection.AddKeyedChatClient("aai-gpt-4o",
+            new AzureAIInferenceChatClient(
+                new Azure.AI.Inference.ChatCompletionsClient(
+                    new Uri(configuration.ǃ("AzureAI:Endpoint")),
+                    new AzureKeyCredential(configuration.ǃ("AzureAI:Key"))),
+                "gpt-4o"));
+        collection.AddKeyedChatClient("aai-gpt-4o-mini",
+            new AzureAIInferenceChatClient(
+                new Azure.AI.Inference.ChatCompletionsClient(
+                    new Uri(configuration.ǃ("AzureAI:Endpoint")),
+                    new AzureKeyCredential(configuration.ǃ("AzureAI:Key"))),
+                "gpt-4o-mini"));
+
+        collection.AddKeyedChatClient("xai-grok-beta",
+            new OpenAI.OpenAIClient(
+                new ApiKeyCredential(configuration.ǃ("xAI:Key")),
+                new OpenAI.OpenAIClientOptions
+                {
+                    Endpoint = new(configuration.ǃ("xAI:Endpoint"))
+                }).AsChatClient("grok-beta"));
+        collection.AddKeyedChatClient("xai-grok-2-latest",
+            new OpenAI.OpenAIClient(
+                new ApiKeyCredential(configuration.ǃ("xAI:Key")),
+                new OpenAI.OpenAIClientOptions
+                {
+                    Endpoint = new(configuration.ǃ("xAI:Endpoint"))
+                }).AsChatClient("grok-2-latest"));
+
+        services = collection.BuildServiceProvider();
     }
 
     [Benchmark]
