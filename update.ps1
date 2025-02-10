8689d5ba9b5ffc9a7660449f1d5e50745aff9b5e
Daniel Cazzulino <daniel@cazzulino.com>
2025-02-10 15:45:45 -0300

    Update script to include projecting a summary

--- update.ps1
+++ update.ps1
@@ -1,6 +1,18 @@
 ﻿git pull && `
 pushd .\src\AI.Benchmarks && `
 dotnet run -c Release && `
+jq -r '.HostEnvironmentInfo.ChronometerFrequency.Hertz as $ticks | 
+.Benchmarks[] | 
+{
+    Provider: (({
+        aai: "Azure AI",
+        oai: "OpenAI",
+        xai: "xAI" 
+    })[.Parameters[7:10]] // "Unknown"),
+    Model: .Parameters[11:],
+    # express as seconds to match markdown
+    Mean: ((.Statistics.Mean / $ticks) | . * 10 | floor | . / 1000)
+}' .\BenchmarkDotNet.Artifacts\results\AI.Benchmarks.ModelPerformance-report-full-compressed.json > .\BenchmarkDotNet.Artifacts\results\summary.json && `
 popd && `
 add *.json && `
 add *.md && `
