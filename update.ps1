7b066ca115096f298a25e9be43b3ce66e8e77d41
Daniel Cazzulino <daniel@cazzulino.com>
2025-02-15 21:38:18 -0300

    Run updated stats, update summary format

--- update.ps1
+++ update.ps1
@@ -4,18 +4,21 @@ dotnet run -c Release
 
 if (-not $?) { throw 'Failed to run benchmarks' }
 
-jq -r '.HostEnvironmentInfo.ChronometerFrequency.Hertz as $ticks | 
-.Benchmarks | map(
-{
-    Provider: (({
-        aai: "Azure AI",
-        oai: "OpenAI",
-        xai: "xAI" 
-    })[.Parameters[7:10]] // "Unknown"),
-    Model: .Parameters[11:],
-    # express as seconds to match markdown
-    Mean: ((.Statistics.Mean / $ticks) | . * 10 | floor | . / 1000)
-})' .\artifacts\results\AI.Benchmarks.ModelPerformance-report-full.json > .\artifacts\summary.json
+jq -r '.HostEnvironmentInfo.ChronometerFrequency.Hertz as $ticks 
+| .Benchmarks 
+| map(select((.Parameters 
+  | (contains("-mini") or contains("-beta")) | not)))
+| map({
+  provider: .Parameters[7:10],
+  model: .Parameters[11:],
+  mean: ((.Statistics.Mean / $ticks) | . * 10 | floor | . / 1000)
+}) 
+| reduce .[] as $item ({};
+  . + {($item.provider): {
+    mean: $item.mean,
+    model: $item.model
+  }}
+)' .\artifacts\results\AI.Benchmarks.ModelPerformance-report-full.json > .\artifacts\summary.json
 
 if (-not $?) { throw 'Failed to create summary' }
 
